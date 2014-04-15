d5076f8ac7d1e16ae68390c4829a1def7b23b672
Justin Roberts <justin.roberts@careerbuilder.com>
2014-04-15 10:12:52 -0400

    initial color

--- prompt.sh
+++ prompt.sh
@@ -1,27 +1,31 @@
-EDITED_BRANCH_COLOR=" \[\e[0;31m\]"
-CLEAN_BRANCH_COLOR=" \[\e[0;32m\]"
-NORMAL_COLOR='\[\e[0m\]'
+#!/bin/sh
+EDITED_BRANCH_COLOR='\[\e[0;31m\]'
+CLEAN_BRANCH_COLOR='\[\e[0;32m\]'
+NORMAL_COLOR='\033[33;0m'
+red='\033[33;31m';
+green='\033[33;32m';
+dirty_color='\033[33;35m';
 
 function parse_git_branch {
   ref=$(git symbolic-ref --short HEAD 2> /dev/null) || return
   #echo "(${ref})"
  # echo "\$()"
-  echo "[${ref} $(cleanliness_color)] "
+  echo -e "[${ref} $(cleanliness_color)${NORMAL_COLOR}] "
 }
 
 function cleanliness_color {
   status=$(git status | sed -n '/\(working directory clean\)/p') || return
   if [ -n "${status}" ]; then
-    echo "clean" 
-    # echo ${CLEAN_BRANCH_COLOR}
+    echo -e "${green}clean"
+    #echo ${CLEAN_BRANCH_COLOR}
   else
-    echo "dirty" 
-    # echo ${EDITED_BRANCH_COLOR}
+    echo -e "${red}dirty"
+    #echo ${EDITED_BRANCH_COLOR}
   fi
 }
 
 function normal_color {
-  echo "${NORMAL_COLOR}"
+  echo -e "${NORMAL_COLOR}"
 }
 
 function current_dir_name {
@@ -30,6 +34,4 @@ function current_dir_name {
 
 PS1="\$(date +%H:%M) \$(whoami) \$(parse_git_branch)$(normal_color)\$(current_dir_name):$ "
 
-#PS1='`date +%H:%M` `whoami` \[`cleanliness_color`\]`parse_git_branch``normal_color``current_dir_name`:$ '
-#PS1="\$(__git_ps1 \"(%s)\")"
 
