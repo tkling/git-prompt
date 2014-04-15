1a1689802564fd31889d4b78020e1897e58a9f64
Justin Roberts <justin.roberts@careerbuilder.com>
2014-04-15 10:19:04 -0400

    better naming

--- prompt.sh
+++ prompt.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
-EDITED_BRANCH_COLOR='\[\e[0;31m\]'
-CLEAN_BRANCH_COLOR='\[\e[0;32m\]'
-NORMAL_COLOR='\033[33;0m'
 red='\033[33;31m';
 green='\033[33;32m';
-dirty_color='\033[33;35m';
+
+EDITED_BRANCH_COLOR=$red
+CLEAN_BRANCH_COLOR=$red
+
+NORMAL_COLOR='\033[33;0m'
 
 function parse_git_branch {
   ref=$(git symbolic-ref --short HEAD 2> /dev/null) || return
-  #echo "(${ref})"
- # echo "\$()"
   echo -e "[$(cleanliness_color)${ref}${NORMAL_COLOR}] "
 }
 
 function cleanliness_color {
   status=$(git status | sed -n '/\(working directory clean\)/p') || return
   if [ -n "${status}" ]; then
-    echo -e "${green}"
-    #echo ${CLEAN_BRANCH_COLOR}
+    echo -e "${EDITED_BRANCH_COLOR}"
   else
-    echo -e "${red}"
-    #echo ${EDITED_BRANCH_COLOR}
+    echo -e "${CLEAN_BRANCH_COLOR}"
   fi
 }
 
